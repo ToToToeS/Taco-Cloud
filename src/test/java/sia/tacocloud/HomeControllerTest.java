25f0d6f101feafaa055ea03ce504b9453af7c945
Shatilo AI <rachinacom@mail.ru>
2025-03-26 22:16:46 +0300

    new commit

--- src/test/java/sia/tacocloud/HomeControllerTest.java
+++ src/test/java/sia/tacocloud/HomeControllerTest.java
@@ -6,13 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(HomeController.class)
 public class HomeControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
     @Test
     public void testHomePage() throws Exception {
         mockMvc.perform(get("/"))
@@ -21,3 +21,4 @@ public class HomeControllerTest {
                 .andExpect(content().string(
                         containsString("Welcome to...")));
     }
+}
